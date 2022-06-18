b365af563557e465608e3f2051a8788f667ee138
Thiago de Sales <thiagobrunoms@gmail.com>
2022-06-18 19:41:08 -0300

    Create second solution option for user entity based on phone number value object

---
+++ lib/bloaters/primitive_obserssion/value_objects/user_fixed2.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_code_smells/bloaters/primitive_obserssion/value_objects/phone_value_object.dart';
+
+class User {
+  PhoneValueObject phoneNumber;
+
+  User({required this.phoneNumber});
+}
