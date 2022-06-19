daf055d7a313c90bb2b11276f140fdabf042857f
Thiago de Sales <thiagobrunoms@gmail.com>
2022-06-19 14:07:55 -0300

    Minor changes

--- lib/bloaters/primitive_obserssion/user_fixed1.dart
+++ lib/bloaters/primitive_obserssion/user_fixed1.dart
@@ -1,8 +1,13 @@
-class PhoneNumber {
+import 'package:equatable/equatable.dart';
+
+class PhoneNumber extends Equatable {
   String ddd;
   String number;
 
   PhoneNumber({required this.ddd, required this.number});
+
+  @override
+  List<Object?> get props => [ddd, number];
 }
 
 class User {
