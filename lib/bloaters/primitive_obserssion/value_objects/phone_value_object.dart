0fbba5bfe6819f043d687b0b535dfa82dd257b6a
Thiago de Sales <thiagobrunoms@gmail.com>
2022-06-19 14:04:17 -0300

    Fix when comparing phone number value objects

--- lib/bloaters/primitive_obserssion/value_objects/phone_value_object.dart
+++ lib/bloaters/primitive_obserssion/value_objects/phone_value_object.dart
@@ -3,9 +3,9 @@ import 'package:flutter_code_smells/bloaters/primitive_obserssion/user_fixed1.da
 import 'package:flutter_code_smells/bloaters/primitive_obserssion/value_objects/phone_failure.dart';
 import 'package:flutter_code_smells/bloaters/primitive_obserssion/value_objects/value_object.dart';
 
-class PhoneValueObject extends ValueObject<PhoneNumber> {
+class PhoneValueObject extends ValueObject<String> {
   @override
-  final Either<PhoneFailure, PhoneNumber> value;
+  final Either<PhoneFailure, String> value;
 
   PhoneValueObject._(this.value);
 
@@ -13,21 +13,28 @@ class PhoneValueObject extends ValueObject<PhoneNumber> {
     return PhoneValueObject._(validatePhone(ddd, phone));
   }
 
-  static Either<PhoneFailure, PhoneNumber> validatePhone(
-      String ddd, String phone) {
+  static Either<PhoneFailure, String> validatePhone(String ddd, String phone) {
     if (ddd.isEmpty) {
       return left(PhoneFailure(message: "Telefone inválido!"));
     }
 
-    return right(PhoneNumber(ddd: ddd, number: phone));
+    return right(ddd + phone);
   }
 }
 
 void main() {
-  PhoneValueObject phone = PhoneValueObject.fromString("", "9999431690");
+  PhoneValueObject phone = PhoneValueObject.fromString("82", "9999431690");
+  PhoneValueObject phone2 = PhoneValueObject.fromString("82", "9999431690");
+
   if (phone.isValid) {
     print(phone.getOrCrash);
   } else {
     print(phone.getOrCrash.message);
+  }
+
+  if (phone == phone2) {
+    print('They are equals!!');
+  } else {
+    print('They are not equals!');
   }
 }
--- lib/bloaters/primitive_obserssion/value_objects/value_object.dart
+++ lib/bloaters/primitive_obserssion/value_objects/value_object.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_code_smells/bloaters/primitive_obserssion/value_objects/errors.dart';
 import 'package:flutter_code_smells/bloaters/primitive_obserssion/value_objects/object_failure.dart';
 
 abstract class ValueObject<T> extends Equatable {
@@ -12,4 +11,23 @@ abstract class ValueObject<T> extends Equatable {
 
   @override
   List<Object?> get props => [value];
+
+  // @override
+  // bool operator ==(Object other) {
+  //   print('checking equals');
+  //   print('identical ${identical(this, other)}');
+  //   print('other is ValueObject ${other is ValueObject}');
+  //   print(
+  //       'runtimeType == other.runtimeType ${runtimeType == other.runtimeType}');
+  //   print(
+  //       'getOrCrash == other.getOrCrash ${other is ValueObject && getOrCrash == other.getOrCrash}');
+
+  //   return identical(this, other) ||
+  //       other is ValueObject &&
+  //           runtimeType == other.runtimeType &&
+  //           getOrCrash == other.getOrCrash;
+  // }
+
+  // @override
+  // int get hashCode => getOrCrash.hashCode;
 }
